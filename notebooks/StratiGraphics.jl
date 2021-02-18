e86b6eada517ae120ee6ee17d5d3196f15c13e6b
Gaurav Wasnik <55525317+cyborg1995@users.noreply.github.com>
2021-02-18 11:38:43 +0530

    fix StratiGraphics.jl and update Pluto version in all notebooks

--- notebooks/StratiGraphics.jl
+++ notebooks/StratiGraphics.jl
@@ -4,70 +4,87 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 01c88650-1fa3-11eb-1156-bb632230292b
-using Random; Random.seed!(2019); # make sure this tutorial is reproducible
-
-# ╔═╡ 221618e0-6e73-11eb-047e-3b329eefa7d3
+# ╔═╡ 2b9ebaaa-1f9c-11eb-39ff-a5ee1ecb90ee
 begin
-	using GeoStats
-	using StratiGraphics
-	using Plots
+	using Distributed
+	pids = [myid()]
 	
-	solver₁ = FFTGS(:land => (variogram=GaussianVariogram(range=100.,sill=3e-2),))
-	solver₂ = FFTGS(:land => (variogram=GaussianVariogram(range=100.,sill=3e-2),))
+	md"""
+	Running on processes: $pids
 	
-	procs = [GeoStatsProcess(solver) for solver in [solver₁, solver₂]];
+	Use `pids = addprocs(n)` to run the notebook with `n` parallel processes.
+	"""
 end
 
-# ╔═╡ cc7eef10-6e72-11eb-2cd4-abf33bd7f1ad
-md"""
-# Installation & Setup
+# ╔═╡ a47c3e6d-9687-4bb9-b71f-3ee242ede575
+@everywhere pids begin
+	using Pkg; Pkg.activate(@__DIR__)
+	Pkg.instantiate(); Pkg.precompile()
+end
 
-Before we proceed, please install the following packages:
-"""
+# ╔═╡ fc7b9a21-34dd-40ba-9d57-9f785904e307
+@everywhere pids begin
+	# packages used in this notebook
+	using GeoStats
+	using StratiGraphics
+	
+	# default plot settings
+	using Plots; gr(size=(700,400))
+	
+	# make sure that results are reproducible
+	using Random; Random.seed!(2021)
+end
 
-# ╔═╡ 6c9b41d8-043f-4626-8798-4a3ae9da922b
+# ╔═╡ b3fa8609-b27e-4ad2-9836-b7841d6a8db0
 md"""
-# Creating surfaces
+## Creating surfaces
 
 First, we demonstrate how Markov-Poisson sampling can be used to generate random surfaces of a 3D stratigraphic model.
 
 We start by defining a set of 2D geostatistical processes:
 """
 
-# ╔═╡ 34e7730e-6e73-11eb-14e4-9f7bfe169470
+# ╔═╡ 61daf6c2-71a3-11eb-3810-ff67b544d72b
+begin
+	solver₁ = FFTGS(:land => (variogram=GaussianVariogram(range=100.,sill=3e-2),))
+	solver₂ = FFTGS(:land => (variogram=GaussianVariogram(range=100.,sill=3e-2),))
+	
+	procs = [GeoStatsProcess(solver) for solver in [solver₁, solver₂]];
+end
+
+# ╔═╡ 700cff42-71a3-11eb-0bd0-43662099751a
 md"""
 In this example, we define two Gaussian processes. This processes represent smooth "depositional" processes as indicated by the GaussianVariogram model. The range and sill parameters determine the "frequency" and "height" of oscillation in the horizons, respectively.
 
 Having a set of processes defined to evolve a landscape, we need transition probabilities between these processes in order to build stratigraphy over time. Here we say that the two processes are balanced (i.e. 50% / 50% chance in the long run):
 """
 
-# ╔═╡ 497c2820-6e73-11eb-3b09-4facec99f59a
+# ╔═╡ 7cfdd530-71a3-11eb-212f-5f05c6e9a32b
 P = [0.5 0.5
      0.5 0.5]
 
-# ╔═╡ 519a6760-6e73-11eb-2987-fd6e15456f15
+# ╔═╡ 8195379e-71a3-11eb-007f-556d235edf2e
 md"""
 Finally, we specify a duration process that determines the (random) amount of time that each process takes before a new transition:
 """
 
-# ╔═╡ 6029f930-6e73-11eb-3834-a355dd9550fa
+# ╔═╡ 87c0f5b0-71a3-11eb-2bd9-5b17609aa37e
 ΔT = ExponentialDuration(1.0)
 
-# ╔═╡ 6778b370-6e73-11eb-03fa-738c64b466a1
+# ╔═╡ 8caa48b0-71a3-11eb-13b6-ad167093b2b5
 md"""
 We create a geological environment with the components defined above:
 """
 
-# ╔═╡ 7bc5b120-6e73-11eb-29c1-7de2d8ab37c4
+# ╔═╡ 943ce9c0-71a3-11eb-1f36-c1575df9d64d
 env = Environment(procs, P, ΔT)
 
-# ╔═╡ 8c6fbe80-6e73-11eb-388f-bb64eaf84d96
+# ╔═╡ a39ee6c0-71a3-11eb-37a6-6b10b691816c
 md"""
 Given an initial state (i.e. flat land), and a number of epochs, we can simulate the environment to produce a geological record:
 """
 
-# ╔═╡ ab57bb40-6e73-11eb-208e-b76a2961ad6c
+# ╔═╡ 98ad8a52-71a3-11eb-11f7-697f1f42f72e
 begin
 	nepochs = 10 # number of surfaces to simulate
 	
@@ -76,12 +93,12 @@ begin
 	record = simulate(env, init, nepochs);
 end
 
-# ╔═╡ b419dba0-6e73-11eb-3dd7-1f5a9141066b
+# ╔═╡ ac62b4d0-71a3-11eb-3f18-45756be15fba
 md"""
 From the record, we can extract the surfaces that make the stratigraphic model. Two options are available for stacking the surfaces, they are :erosional (default) in which case the surfaces are eroded backward in time, and :depositional in which case the surfaces are deposited forward in time:
 """
 
-# ╔═╡ bb05dc70-6e73-11eb-0e35-25fb65c02fd4
+# ╔═╡ b43481c0-71a3-11eb-358e-6314ea693e5e
 begin
 	gr(format=:png)
 	
@@ -90,12 +107,12 @@ begin
 	plot(strata, size=(600,600))
 end
 
-# ╔═╡ ccec3a60-6e73-11eb-3bf6-ff8d31deaca2
+# ╔═╡ c1813350-71a3-11eb-1d48-ab432f7a2f4b
 md"""
 We can convert the stratigraphic model into a 3D voxel model by specifying a vertical resolution:
 """
 
-# ╔═╡ e648fed0-6e73-11eb-0907-1d7e194a38c6
+# ╔═╡ ca98edc0-71a3-11eb-25d8-bde416e1b51a
 begin
 	model = voxelize(strata, 250) # 500x500x250 voxel model
 	
@@ -108,26 +125,33 @@ begin
 	plot(px, py, size=(950,200), aspect_ratio=:equal, clim=(0,nepochs))
 end
 
-# ╔═╡ ef7b6d30-6e73-11eb-0427-f7d965c99f8f
+# ╔═╡ d06dd940-71a3-11eb-25ba-5d1cf54e15e2
 md"""
-# Putting it all together
+## Putting it all together
+
 We can create many such stratigraphic models by defining a simulation problem for stratigraphy:
 """
 
-# ╔═╡ fe0288a0-6e75-11eb-1bcb-e12f7baa44cb
+# ╔═╡ e00676a0-71a3-11eb-1355-4b949a460052
 problem = SimulationProblem(RegularGrid{Float64}(500,500,250), :strata => Float64, 3)
 
-# ╔═╡ 08c6ba90-6e76-11eb-059d-6f09ad18a136
+# ╔═╡ e79e47d0-71a3-11eb-1fa7-6da8d2f1a167
+md"""
+The StratSim solver is compliant with the GeoStats.jl API:
+"""
+
+# ╔═╡ ee764b70-71a3-11eb-2d35-43056ba7f528
 solver = StratSim(:strata => (environment=env,))
 
-# ╔═╡ 1a4221b0-6e76-11eb-26e1-6da7b3ddffe3
-begin
-	Random.seed!(2000)
-	
-	solution = solve(problem, solver)
-end
+# ╔═╡ fad1e640-71a3-11eb-1518-0d2eb7b95875
+solution = solve(problem, solver)
 
-# ╔═╡ 21c77c50-6e76-11eb-0621-b1a96184c856
+# ╔═╡ 05b8944e-71a4-11eb-269a-cb9412e8e223
+md"""
+The solution contains 3 realizations of stratrigraphy, which can be visualized with:
+"""
+
+# ╔═╡ fb8cea20-71a9-11eb-2a4c-4beb0a2f20d9
 for (i,real) in enumerate(solution[:strata])
     xslice = rotr90(real[25,:,:])
     yslice = rotr90(real[:,25,:])
@@ -141,24 +165,27 @@ for (i,real) in enumerate(solution[:strata])
 end
 
 # ╔═╡ Cell order:
-# ╟─cc7eef10-6e72-11eb-2cd4-abf33bd7f1ad
-# ╠═01c88650-1fa3-11eb-1156-bb632230292b
-# ╟─6c9b41d8-043f-4626-8798-4a3ae9da922b
-# ╠═221618e0-6e73-11eb-047e-3b329eefa7d3
-# ╟─34e7730e-6e73-11eb-14e4-9f7bfe169470
-# ╠═497c2820-6e73-11eb-3b09-4facec99f59a
-# ╟─519a6760-6e73-11eb-2987-fd6e15456f15
-# ╠═6029f930-6e73-11eb-3834-a355dd9550fa
-# ╟─6778b370-6e73-11eb-03fa-738c64b466a1
-# ╠═7bc5b120-6e73-11eb-29c1-7de2d8ab37c4
-# ╟─8c6fbe80-6e73-11eb-388f-bb64eaf84d96
-# ╠═ab57bb40-6e73-11eb-208e-b76a2961ad6c
-# ╟─b419dba0-6e73-11eb-3dd7-1f5a9141066b
-# ╠═bb05dc70-6e73-11eb-0e35-25fb65c02fd4
-# ╟─ccec3a60-6e73-11eb-3bf6-ff8d31deaca2
-# ╠═e648fed0-6e73-11eb-0907-1d7e194a38c6
-# ╟─ef7b6d30-6e73-11eb-0427-f7d965c99f8f
-# ╠═fe0288a0-6e75-11eb-1bcb-e12f7baa44cb
-# ╠═08c6ba90-6e76-11eb-059d-6f09ad18a136
-# ╠═1a4221b0-6e76-11eb-26e1-6da7b3ddffe3
-# ╠═21c77c50-6e76-11eb-0621-b1a96184c856
+# ╟─2b9ebaaa-1f9c-11eb-39ff-a5ee1ecb90ee
+# ╟─a47c3e6d-9687-4bb9-b71f-3ee242ede575
+# ╠═fc7b9a21-34dd-40ba-9d57-9f785904e307
+# ╟─b3fa8609-b27e-4ad2-9836-b7841d6a8db0
+# ╠═61daf6c2-71a3-11eb-3810-ff67b544d72b
+# ╟─700cff42-71a3-11eb-0bd0-43662099751a
+# ╠═7cfdd530-71a3-11eb-212f-5f05c6e9a32b
+# ╟─8195379e-71a3-11eb-007f-556d235edf2e
+# ╠═87c0f5b0-71a3-11eb-2bd9-5b17609aa37e
+# ╟─8caa48b0-71a3-11eb-13b6-ad167093b2b5
+# ╠═943ce9c0-71a3-11eb-1f36-c1575df9d64d
+# ╟─a39ee6c0-71a3-11eb-37a6-6b10b691816c
+# ╠═98ad8a52-71a3-11eb-11f7-697f1f42f72e
+# ╟─ac62b4d0-71a3-11eb-3f18-45756be15fba
+# ╠═b43481c0-71a3-11eb-358e-6314ea693e5e
+# ╟─c1813350-71a3-11eb-1d48-ab432f7a2f4b
+# ╠═ca98edc0-71a3-11eb-25d8-bde416e1b51a
+# ╟─d06dd940-71a3-11eb-25ba-5d1cf54e15e2
+# ╠═e00676a0-71a3-11eb-1355-4b949a460052
+# ╟─e79e47d0-71a3-11eb-1fa7-6da8d2f1a167
+# ╠═ee764b70-71a3-11eb-2d35-43056ba7f528
+# ╠═fad1e640-71a3-11eb-1518-0d2eb7b95875
+# ╟─05b8944e-71a4-11eb-269a-cb9412e8e223
+# ╠═fb8cea20-71a9-11eb-2a4c-4beb0a2f20d9
